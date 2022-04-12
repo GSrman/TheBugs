f44afa3191db3c596e1fe175a6662977cff3386c
DBPoot <dbpoot@gmail.com>
2022-04-12 23:42:34 +0200

    Added last data set

--- process.py
+++ process.py
@@ -34,14 +34,25 @@ def process():
 
         words = tokenize_clean(sentences) # list: all tokenized words
         word_cnt = len(words) # int: amount of words in text
-        syllable_cnt = count_syl(words)[0] # int: amount of syllables in text
+        syllable_cnt, long_words_syll = count_syl(words) # int: amount of syllables in text
         par_cnt = count_par(text) # int: amount of paragraphs
         char_cnt = count_char(text) # int: amount of characters
         word_per_par = word_cnt/par_cnt # float: average amount of words per paragraph
 
         avg_sent_word = word_cnt/sent_cnt  # float: average words per sentence
-        avg_word_char = count_chars(words)[1]  # float: average characters per word
+        chars_cnt, avg_word_char, long_words_char = count_chars(words)  # float: average characters per word
         avg_word_syll = syllable_cnt/word_cnt  # float: average syllables per word
+
+        extra_long_sents, long_sents = get_long_sent(sentences)
+        extra_long_sent_cnt = len(extra_long_sents)
+        extra_long_sent_pct = math.floor(((extra_long_sent_cnt/sent_cnt)*100))
+        long_sent_cnt = len(long_sents)
+        long_sent_pct = math.floor(((long_sent_cnt/sent_cnt)*100))
+
+        long_words_syll_cnt = len(long_words_syll)
+        long_words_syll_pct = math.floor(((long_words_syll_cnt/word_cnt)*100))
+        long_words_char_cnt = len(long_words_char)
+        long_words_char_pct = math.floor(((long_words_char_cnt/word_cnt)*100))
 
         # compute some readability scores
         # Leesindex R.H.M. Brouwer (1963)
@@ -81,6 +92,11 @@ def process():
                                   '<tr><td title="~15-20 is het best">Woorden per zin gemiddeld</td><td>'+str(round(avg_sent_word, 1))+'</td></tr>'+
                                   '<tr><td title="~11.5 is het best">Karakters per woord gemiddeld</td><td>'+add_span("word_char", round(avg_word_char, 1))+'</td></tr>'+
                                   '<tr><td title="<=3 is het best">Lettergrepen per woord gemiddeld</td><td>'+add_span("word_syll", round(avg_word_syll, 1))+'</td></tr>'+
+                                  '<tr><th>Te Lange Woorden/Zinnen</th></tr>'+
+                                  '<tr><td>Erg lange zinnen(Lettergrepen>30)</td><td>'+str(extra_long_sent_cnt)+" ("+str(extra_long_sent_pct)+'%)</td></tr>'+
+                                  '<tr><td>lange zinnen(Lettergrepen>20)</td><td>'+str(long_sent_cnt)+" ("+str(long_sent_pct)+'%)</td></tr>'+
+                                  '<tr><td>Lange Woorden(Lettergrepen>4)</td><td>'+str(long_words_syll_cnt)+" ("+str(long_words_syll_pct)+'%)</td></tr>'+
+                                  '<tr><td>Lange Woorden(Karakters>12)</td><td>'+str(long_words_char_cnt)+" ("+str(long_words_char_pct)+'%)</td></tr>'+
                                   '<tr><th>Leesbaarheid</th></tr>'+
                                   '<tr><td title="0-120, lager is moeilijker">Brouwer Index</td><td>'+str(round(brouwer_index, 1))+'</td></tr>'+
                                   '<tr><td>AVI Niveau</td><td>'+str(avi_level)+'</td></tr>'+
